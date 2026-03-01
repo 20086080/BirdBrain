cf8b4315908964a1783079993010f24923101757
20086080 <20086080@tafe.wa.edu.au>
2026-03-01 15:12:09 +0530

    Feat : Bird Images for Carousel

--- BirdBrain/Views/BirdSelectionPage.xaml.cs
+++ BirdBrain/Views/BirdSelectionPage.xaml.cs
@@ -12,10 +12,10 @@ public partial class BirdSelectionPage : BasePage
 
         BirdCarousel.ItemsSource = new List<Bird>
         {
-            new Bird { Name = "Parrot", Image = "parrot.png" },
-            new Bird { Name = "Cuckoo", Image = "sparrow.png" },
-            new Bird { Name = "Sparrow", Image = "parrot.png" },
-            new Bird { Name = "Kingfisher", Image = "sparrow.png" }
+            new Bird { Name = "Garzette", Image = "aigrette_garzette_au_lac_sud_de_tunis_t.png" },
+            new Bird { Name = "Attis", Image = "alcedo_atthis_t.png" },
+            new Bird { Name = "Asian Koel", Image = "asian_koel_t.png" },
+            new Bird { Name = "Black Drongo", Image = "black_drongo_in_purulia_t.png" }
         };
 
     }

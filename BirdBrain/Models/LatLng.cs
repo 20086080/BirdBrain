278f142f1fdff64053902cb75db1b3b16a8b35fd
20086080 <20086080@tafe.wa.edu.au>
2026-03-08 19:15:12 +0530

    Feat : LatLng (Latitude Longitude) Master Data

---
+++ BirdBrain/Models/LatLng.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Text;
+
+namespace BirdBrain.Models
+{
+    public class LatLng
+    {
+        [JsonPropertyName("city_ascii")]
+        public string CityAscii { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double Lat { get; set; }
+
+        [JsonPropertyName("lng")]
+        public double Lng { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("iso2")]
+        public string Iso2 { get; set; }
+
+        public string Display =>
+        $"{CityAscii}, {Country} ({Lat}, {Lng})";
+    }
+}
---
+++ BirdBrain/Services/LatLngService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BirdBrain.Models;
+using BirdBrain.Services;
+
+namespace BirdBrain.Services
+{
+    public class LatLngService
+    {
+        private readonly JsonFileReader _jsonReader;
+
+        public List<LatLng> LatLng { get; private set; }
+
+        public LatLngService(JsonFileReader jsonReader)
+        {
+            _jsonReader = jsonReader;
+        }
+
+        public async Task LoadAsync()
+        {
+            if (LatLng != null)
+                return;
+
+            LatLng = await _jsonReader.ReadListAsync<LatLng>("LatLngSeedData.json");
+        }
+    }
+}
+

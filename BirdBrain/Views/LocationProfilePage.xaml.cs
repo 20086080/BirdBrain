8dfbc961b0665dd6dea74b0b61c6ba676a93a7ac
20086080 <20086080@tafe.wa.edu.au>
2026-03-17 21:40:46 +0530

    Feat : Location Profile Page

--- BirdBrain/Views/LocationProfilePage.xaml.cs
+++ BirdBrain/Views/LocationProfilePage.xaml.cs
@@ -1,9 +1,71 @@
+using BirdBrain.Services;
+using BirdBrain.Models;
+using System.Diagnostics;
 namespace BirdBrain.Views;
 
-public partial class LocationProfilePage : ContentView
+public partial class LocationProfilePage : BasePage
 {
-	public LocationProfilePage()
+    private readonly SummaryService _summaryService;
+    public LocationProfilePage(SummaryService summaryService)
 	{
 		InitializeComponent();
-	}
+        _summaryService = summaryService;
+        App.State.LeftSelected = true;
+        BindingContext = App.State;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
+    async void LocationTapped(object sender, EventArgs e)
+    {
+        try
+        {
+            LocationTab.Style =
+                (Style)Application.Current.Resources["SegmentSelectedStyle"];
+
+            BirdTab.Style =
+                (Style)Application.Current.Resources["SegmentUnselectedStyle"];
+
+            LocationTabLabel.Style =
+                (Style)Application.Current.Resources["SegmentSelectedLabelStyle"];
+
+            BirdTabLabel.Style =
+                (Style)Application.Current.Resources["SegmentUnselectedLabelStyle"];
+            App.State.LeftSelected = true;
+            await Shell.Current.GoToAsync(nameof(LocationSightingPage));
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            throw;
+        }
+    }
+
+    async void BirdTapped(object sender, EventArgs e)
+    {
+        try
+        {
+            BirdTab.Style =
+                (Style)Application.Current.Resources["SegmentSelectedStyle"];
+
+            LocationTab.Style =
+                (Style)Application.Current.Resources["SegmentUnselectedStyle"];
+
+            BirdTabLabel.Style =
+                (Style)Application.Current.Resources["SegmentSelectedLabelStyle"];
+
+            LocationTabLabel.Style =
+                (Style)Application.Current.Resources["SegmentUnselectedLabelStyle"];
+            App.State.LeftSelected = false;
+            //Navigate using Shell
+            await Shell.Current.GoToAsync(nameof(BirdSightingPage));
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            throw;
+        }
+    }
 }
